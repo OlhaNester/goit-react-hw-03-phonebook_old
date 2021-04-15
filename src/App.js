e7691f99dba06c266e78a9f74d3ef0ef80f80cdb
OlhaNester <olga_nv@ukr.net>
2021-04-15 21:20:51 +0300

    111

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
-import { Component } from 'react';
-import './App.css';
-import Container from './components/Container';
-import Form from './components/Form';
-import ContactList from './components/ContactList';
-import Filter from './components/Filter';
-//import shortid from 'shortid';
+import { Component } from "react";
+import "./App.css";
+import Container from "./components/Container";
+import Form from "./components/Form";
+import ContactList from "./components/ContactList";
+import Filter from "./components/Filter";
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
+    contacts: [],
+    filter: "",
   };
 
   //добавляет контакт
@@ -27,7 +21,7 @@ class App extends Component {
     };
     if (
       contacts.find(
-        contact => contact.name.toLowerCase() === name.toLowerCase(),
+        (contact) => contact.name.toLowerCase() === name.toLowerCase()
       )
     ) {
       alert(`${name} is already in contacts`);
@@ -38,23 +32,41 @@ class App extends Component {
   };
 
   // удаляет контакт
-  deleteContact = contactId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+  deleteContact = (contactId) => {
+    this.setState((prevState) => ({
+      contacts: prevState.contacts.filter(
+        (contact) => contact.id !== contactId
+      ),
     }));
   };
   //значение фильтра записываем в стейт
-  changeFilter = event => {
+  changeFilter = (event) => {
     this.setState({ filter: event.currentTarget.value });
   };
   //фильтрует контакты
   getFilterContact = () => {
     const { contacts, filter } = this.state;
     const normaFilter = filter.toLowerCase();
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normaFilter),
+    return contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(normaFilter)
     );
   };
+  // вызывается один раз при загрузке
+  componentDidMount() {
+    const contacts = localStorage.getItem("contacts");
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  //вызывается после изменения состояния или свойств
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
+    }
+  }
 
   render() {
     const { filter } = this.state;
